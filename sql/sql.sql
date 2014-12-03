506d7efdc1ae75155577b90f104cc90f2e20a7bf
unknown <dei@43-dei.bintang.com>
2014-12-03 08:27:40 +0700

    master_table_update

--- sql/sql.sql
+++ sql/sql.sql
@@ -1,4 +1,4 @@
-drop table if exists users;
+﻿drop table if exists users;
 CREATE TABLE users (
 id serial primary key,
 time_stamp timestamp default now(),
@@ -49,4 +49,106 @@ time_stamp timestamp default now(),
 created_by varchar default current_user,
 user_id int,
 role_id int
+);
+
+---------------------------------------------		--+
+drop table if exists customers;
+create table customers (
+id serial primary key,
+time_stamp timestamp default now(),
+created_by varchar default current_user,
+customer_code varchar not null unique,			--+
+customer_name varchar not null,
+full_address varchar(1000),				--+
+province int,
+city int,
+email varchar,
+website varchar,
+customer_since date
+);
+							--+
+drop table if exists customer_pics;
+create table customer_pics (
+id serial primary key,
+time_stamp timestamp default now(),
+created_by varchar default current_user,		--+
+customer_id int not null,
+pic_name varchar,
+role varchar,
+religion varchar, -- because religoion can be very vary
+birthday date,
+fix_phone varchar,
+mobile_phone varchar,
+email varchar
+);
+--------------------------------------------------	--+
+
+
+-- related to --------------------------------------------+
+drop table if exists suppliers;				--+
+create table suppliers  (
+id serial primary key,
+time_stamp timestamp default now(),
+created_by varchar default current_user,		--+
+supplier_code varchar,
+supplier_name varchar,
+address varchar,
+province int,						--+
+city int,
+fix_phone varchar,
+fax varchar,
+mobile_phone varchar,
+email varchar,
+website varchar
+);
+							--+
+drop table if exists supplier_pics;
+create table supplier_pics (
+id serial primary key,
+time_stamp timestamp default now(),
+created_by varchar default current_user,		--+
+supplier_id int,
+pic_name varchar,
+fix_phone varchar,
+extensions varchar,
+mobile_phone varchar,					--+
+email varchar
+);
+----------------------------------------------------	--+		
+
+drop table if exists events;
+create table  events ( 
+id serial primary key,
+time_stamp timestamp default now(),
+created_by varchar default current_user,
+event_name varchar,
+industry varchar,
+email varchar,
+website varchar
+);
+
+drop table if exists venues;
+create table venues (
+id serial primary key,
+time_stamp timestamp default now(),
+created_by varchar default current_user,
+venue_name varchar,
+address varchar,
+province int,
+city int,
+email varchar,
+website varchar
+);
+
+drop table if exists warehouses ;
+create table warehouses (
+id serial primary key,
+time_stamp timestamp default now(),
+created_by varchar default current_user,
+warehouse_name varchar,
+adress varchar,
+province int,
+city int,
+fix_phone varchar,
+mobile_phone varchar
 );
