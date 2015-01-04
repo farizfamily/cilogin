583b8a4506afaaadaf55923089274a0bae461d51
unknown <dei@.(none)>
2015-01-04 17:03:01 +0700

    modif some data type and add remove some column

--- sql/sql.sql
+++ sql/sql.sql
@@ -1,6 +1,7 @@
-﻿drop table if exists users;
+
+drop table if exists users;
 CREATE TABLE users (
-id serial primary key,
+user_id serial primary key,
 time_stamp timestamp default now(),
 created_by varchar default current_user,
 register_number varchar unique,
@@ -16,7 +17,7 @@ insert into users  ( userid,username,email,passw) values ('dei','Ridei Karim','r
 
 drop table if exists roles;
 create table roles (
-id serial primary key,
+role_id serial primary key,
 time_stamp timestamp default now(),
 created_by varchar default  current_user,
 role_name varchar
@@ -24,7 +25,7 @@ role_name varchar
 
 drop table if exists menus ;
 create table menus (
-id serial primary key,
+menu_id serial primary key,
 time_stamp timestamp default now(),
 created_by varchar default current_user,
 menu_name varchar,
@@ -54,7 +55,7 @@ role_id int
 ---------------------------------------------		--+
 drop table if exists customers;
 create table customers (
-id serial primary key,
+customer_id serial primary key,
 time_stamp timestamp default now(),
 created_by varchar default current_user,
 customer_code varchar not null unique,			--+
@@ -69,25 +70,29 @@ customer_since date
 							--+
 drop table if exists customer_pics;
 create table customer_pics (
-id serial primary key,
+customer_pic_id serial primary key,
 time_stamp timestamp default now(),
 created_by varchar default current_user,		--+
 customer_id int not null,
 pic_name varchar,
-role varchar,
+role_id varchar,
 religion varchar, -- because religoion can be very vary
 birthday date,
 fix_phone varchar,
 mobile_phone varchar,
 email varchar
 );
+
+ALTER TABLE ONLY customer_pics
+    ADD CONSTRAINT customer_pic_maps FOREIGN KEY (customer_id) REFERENCES customers(customer_id) ON UPDATE RESTRICT ON DELETE RESTRICT;
+
 --------------------------------------------------	--+
 
 
 -- related to --------------------------------------------+
 drop table if exists suppliers;				--+
 create table suppliers  (
-id serial primary key,
+supplier_id serial primary key,
 time_stamp timestamp default now(),
 created_by varchar default current_user,		--+
 supplier_code varchar,
@@ -104,7 +109,7 @@ website varchar
 							--+
 drop table if exists supplier_pics;
 create table supplier_pics (
-id serial primary key,
+suplier_pic_id serial primary key,
 time_stamp timestamp default now(),
 created_by varchar default current_user,		--+
 supplier_id int,
@@ -114,11 +119,12 @@ extensions varchar,
 mobile_phone varchar,					--+
 email varchar
 );
+alter table supplier_pics    ADD CONSTRAINT supplier_pic_maps FOREIGN KEY (supplier_id) REFERENCES suppliers(supplier_id) ON UPDATE RESTRICT ON DELETE RESTRICT;
 ----------------------------------------------------	--+		
 
 drop table if exists events;
 create table  events ( 
-id serial primary key,
+event_id serial primary key,
 time_stamp timestamp default now(),
 created_by varchar default current_user,
 event_name varchar,
@@ -129,7 +135,7 @@ website varchar
 
 drop table if exists venues;
 create table venues (
-id serial primary key,
+venue_id serial primary key,
 time_stamp timestamp default now(),
 created_by varchar default current_user,
 venue_name varchar,
@@ -142,7 +148,7 @@ website varchar
 
 drop table if exists warehouses ;
 create table warehouses (
-id serial primary key,
+warehouse_id serial primary key,
 time_stamp timestamp default now(),
 created_by varchar default current_user,
 warehouse_name varchar,
@@ -152,3 +158,56 @@ city int,
 fix_phone varchar,
 mobile_phone varchar
 );
+
+-- quotation
+-- quotation status 
+-- created, this the first when user entry, it just saved not more
+-- ok, 
+	-- since only approved quotation entered to system, then ok means the quotation in force, 
+	-- once quotation status turn into ok, then the user only have one chance to update to become win or loss status
+-- win
+-- loss
+
+drop table if exists quotations ;
+create table quotations (
+quotation_id serial primary key,
+time_stamp timestamp default now(),
+created_by varchar default current_user,
+quotation_number varchar,
+status int, -- start from 1 means draft, 2 means in force, 3 win, 4 loss
+revision varchar,
+sales_number varchar, -- fill this column means the quotation turn into sales
+quotation_date date,
+contract_number varchar,
+term_of_payment varchar,
+construction_type varchar,
+size varchar,
+official boolean,
+event_id integer,
+venue_id integer,
+show_day_from timestamp,
+show_day_to timestamp,
+move_in_from timestamp,
+move_in_to timestamp,
+move_out_from timestamp,
+move_out_to timestamp,
+customer_id integer,
+customer_contacts varchar,
+project_executive integer,
+project_supervisor integer,
+designer integer,
+notes text
+);
+
+drop table if exists quotation_products ;
+create table quotation_products (
+quotation_product_id serial primary key,
+time_stamp timestamp default now(),
+created_by varchar default current_user,
+quotation_id int,
+product_group varchar,
+description varchar,
+amount numeric(20,2)
+);
+
+
